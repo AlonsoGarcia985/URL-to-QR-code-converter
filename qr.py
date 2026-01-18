c0eac0ff2c01d8ab5e204c9222639b871d6c4c46
AlonsoGarcia985 <alonsogarciam04@gmail.com>
2026-01-17 21:36:30 -0600

    Update qr.py

--- qr.py
+++ qr.py
@@ -1,6 +1,6 @@
 import qrcode
 
-url = "https://electrofarfanes.mx/eq1/AyV/Proyectito/login.html"
+url = "www.google.com"
 
 qr = qrcode.QRCode(
     version=1,
@@ -14,5 +14,6 @@ qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white")
 img.save("qr_url.png")
+
 
 print("QR generado: qr_url.png")
